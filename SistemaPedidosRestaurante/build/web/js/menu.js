85c2f46af58fd44a0782e08e225d0ad23ba5094c
sjhilmar <sjalvarezt9627@gmail.com>
2022-09-16 16:19:51 -0500

    proyecto

---
+++ SistemaPedidosRestaurante/build/web/js/menu.js
@@ -0,0 +1,9 @@
+
+let btnMenu = document.getElementById('btnmenu');
+let menu=document.getElementById('menu');
+
+btnMenu.addEventListener('click',function(){
+    'use strict';
+    menu.classList.toggle('mostrar');
+});
+
