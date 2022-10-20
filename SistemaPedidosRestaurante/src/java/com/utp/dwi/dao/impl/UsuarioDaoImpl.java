a65b25f9b46f70880cd92e24408c343e1515714c
sjhilmar <sjalvarezt9627@gmail.com>
2022-10-19 20:31:54 -0500

    listaProducto

---
+++ SistemaPedidosRestaurante/src/java/com/utp/dwi/dao/impl/UsuarioDaoImpl.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.utp.dwi.dao.impl;
+
+import com.utp.dwi.bean.UsuarioBean;
+import com.utp.dwi.dao.IUsuarioDao;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author JMR CODE
+ */
+public class UsuarioDaoImpl implements IUsuarioDao {
+
+    @Override
+    public List<UsuarioBean> Listar(String descripcion) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public UsuarioBean insertar(UsuarioBean producto) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public UsuarioBean actualizar(UsuarioBean producto) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void eliminar(int UserID) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
